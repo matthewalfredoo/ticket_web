9f366b9ff27cd538e08ee533d2cca099673fb0d5
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-06-26 16:09:09 +0700

    Revert "renew transaksis table migration"
    This reverts commit bc6b158fd8f74175be1998ad546fd54c05784202.

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -7,7 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     protected $filable = [
-        'user_id', 'jumlah', 'harga', 'tanggal', 'bukti_transfer', 'status'
+        'user_id', 'kode_payment', 'kode_trx', 'total_item', 'total_harga', 'kode_unik',
+        'status', 'name', 'phone', 'detail_tanggal', 'deskripsi', 'metode', 'total_transfer',
+        'bank', 'expired_at'
     ];
 
+    public function details(){
+        return $this->hasMany(TransaksiDetail::class,"transaksi_id","id");
+    }
+
+    public function user(){
+        return $this -> belongsTo(User::class, "user_id", "id");
+    }
 }
